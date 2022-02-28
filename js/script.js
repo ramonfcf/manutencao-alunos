d8f6f58b6555ca07a2c21ed9d20a69b5ba61e48e
Ramon <ramonfcf@gmail.com>
2022-02-27 22:23:01 -0300

    calculo de média e escrita automática no html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-var alunos = document.querySelectorAll(".tabela-alunos");
+var alunos = document.querySelectorAll(".aluno");
 
-console.log(alunos)
+for(i = 0; i < alunos.length; i++ ){
+    let aluno = alunos[i];
+    
+    var alunoNota1 = aluno.querySelector('.aluno-nota-1');
+    var nota1 = Number(alunoNota1.textContent);
+    //console.log(nota1)
+
+    var alunoNota2 = aluno.querySelector('.aluno-nota-2');
+    var nota2 = Number(alunoNota2.textContent);
+   // console.log(nota2)
+
+
+    var alunoNota3 = aluno.querySelector('.aluno-nota-3');
+    var nota3 = Number(alunoNota3.textContent);
+    //    console.log(nota3)
+
+
+    var alunoNota4 = aluno.querySelector('.aluno-nota-4');
+    var nota4 = Number(alunoNota4.textContent);
+    //console.log(nota4)
+
+    var alunoMedia = aluno.querySelector('.aluno-media');
+
+    var mediaNota = (nota1 + nota2 + nota3 + nota4) / 4;
+    
+    alunoMedia.textContent = mediaNota
+
+    var alunoSituacao = aluno.querySelector('.aluno-situacao');
+
+    if (mediaNota < 6){
+        alunoSituacao.textContent = "Reprovado";
+    } else {
+        alunoSituacao.textContent = "Aprovado";
+    }
+}
+
